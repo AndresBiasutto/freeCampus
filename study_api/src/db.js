e397b99797cd6c08877b934c6ff3bcfd2497ec43
AndresBiasutto <aquiandresbiasutto@gmail.com>
2024-07-16 21:12:07 -0300

    intento de de ploy

--- study_api/src/db.js
+++ study_api/src/db.js
@@ -2,9 +2,16 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
-const { DATABASE_URL } = process.env;
+const { DATABASE_URL, //local
+        POSTGRE_DB  //deploy
+   } = process.env;
 
-const sequelize = new Sequelize(DATABASE_URL, {
+const sequelize = new Sequelize(POSTGRE_DB, {
+  dialectOptions: {
+    ssl:{require:true,
+         rejectUnauthorized: false,
+    },
+  },
   logging: false,
   native: false,
 });
@@ -14,7 +21,10 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 fs.readdirSync(path.join(__dirname, "/models"))
-  .filter((file) => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js")
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
@@ -25,16 +35,16 @@ const { User, Subject, File, Role, Module, Video } = sequelize.models;
 
 // Associations
 User.belongsTo(Role, { foreignKey: "role" });
-User.belongsToMany(Subject, { through: 'UserSubject', as: 'enrolledSubjects' });
-Subject.belongsToMany(User, { through: 'UserSubject', as: 'students' });
+User.belongsToMany(Subject, { through: "UserSubject", as: "enrolledSubjects" });
+Subject.belongsToMany(User, { through: "UserSubject", as: "students" });
 File.belongsTo(Module, { foreignKey: "moduleId", as: "Module" });
-Module.hasMany(File, { foreignKey: 'moduleId', as: 'Files' });
-Subject.hasMany(Module, { foreignKey: 'subjectId' });
-Module.belongsTo(Subject, { foreignKey: 'subjectId' });
-User.hasMany(Subject, { as: 'createdSubjects', foreignKey: 'creatorId' });
-Subject.belongsTo(User, { as: 'creator', foreignKey: 'creatorId' });
-Module.belongsTo(Video, { foreignKey: 'videoId' });
-Video.hasOne(Module, { foreignKey: 'videoId' });
+Module.hasMany(File, { foreignKey: "moduleId", as: "Files" });
+Subject.hasMany(Module, { foreignKey: "subjectId" });
+Module.belongsTo(Subject, { foreignKey: "subjectId" });
+User.hasMany(Subject, { as: "createdSubjects", foreignKey: "creatorId" });
+Subject.belongsTo(User, { as: "creator", foreignKey: "creatorId" });
+Module.belongsTo(Video, { foreignKey: "videoId" });
+Video.hasOne(Module, { foreignKey: "videoId" });
 
 module.exports = {
   ...sequelize.models,
