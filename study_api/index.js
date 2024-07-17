6b5fbfd0aac1fa9240341be8974949873c88dc55
AndresBiasutto <aquiandresbiasutto@gmail.com>
2024-07-16 21:27:40 -0300

    segundo intento deploy

--- study_api/index.js
+++ study_api/index.js
@@ -1,12 +1,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {
-  DB_PORT
+  DB_PORT, //PUERTO LOCAL
+  RENDER_PORT //PURTO DE RENDER
 } = process.env;
 
 // Syncing all the models at once. alter o force
 conn.sync({ alter: true }).then(() => {
-  server.listen(DB_PORT, () => {
-    console.log( `Servidor conectado en el puerto ${DB_PORT}`  ); // eslint-disable-line no-console
+  server.listen(RENDER_PORT, () => {
+    console.log( `Servidor conectado en el puerto ${RENDER_PORT}`  ); // eslint-disable-line no-console
   });
 });
